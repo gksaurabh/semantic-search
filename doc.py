9e59d44c76f7f41cae977c03ca43c5f41148676c
Saurabh Kishore <gks210199@gmail.com>
2023-05-10 23:50:36 -0400

    data cleaning complete and json has been parsed

--- doc.py
+++ doc.py
@@ -1,7 +1,32 @@
+from curses import raw
 import spacy
-nlp = spacy.load("en_core_web_sm")
+import json
+nlp = spacy.load("en_core_web_lg")
 
-with open("data/wiki-nlp.txt",'r') as file:
-    text = file.read()
 
-print(text)
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
+
+file = open("data/cie-air-default-rtdb-export.json", 'r')
+raw_data = []
+raw_data.append(file)
+
+
+for element in raw_data:
+    data = json.load(element)
+
+researchInterests = {}
+for item in data:
+    for interest in item["research"].values():
+        researchInterests.update({item["name"]: interest})
+
+print(len(researchInterests))
+for val in researchInterests.values():
+    
+    if(type(val) == list):
+        print(get_keys_from_value(researchInterests,val)[0])
+        del researchInterests[get_keys_from_value(researchInterests,val)[0]]
+        
+print(len(researchInterests))
+
+
